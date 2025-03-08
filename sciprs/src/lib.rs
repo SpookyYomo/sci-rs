83cdb6aac2e38c86d63096f7bd0a9299a09022ff
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-03-08 17:00:09 +0800

    Bind rust library into sciprs module for Python
    With the virtual environment containing maturin, within the sciprs/
    folder, run
    ```
      maturin develop
      pip install -e .
    ```
    Now create a test script with `from sciprs import fftconvolve`, and run
    with the same virtual environment.

--- sciprs/src/lib.rs
+++ sciprs/src/lib.rs
@@ -1,14 +1,15 @@
 use pyo3::prelude::*;
+use sci_rs::signal::convolve::{fftconvolve, ConvolveMode};
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyo3(name = "fftconvolve")]
+fn fftconvolve_py(in1: Vec<f64>, in2: Vec<f64>) -> PyResult<Vec<f64>> {
+    Ok(fftconvolve(&in1, &in2, ConvolveMode::Full))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn sciprs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(fftconvolve_py, m)?)?;
     Ok(())
 }

b11cb41b8e83d5bb03d1c5a567356a35ff9a25df
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-04-13 20:37:34 +0800

    Add upfirdn_apply::extend_right

--- sci-rs/src/signal/filter/upfirdn.rs
+++ sci-rs/src/signal/filter/upfirdn.rs
@@ -90,4 +90,85 @@ pub(self) mod apply {
             }
         }
     }
+
+    // Generic allows for array, ndarray and vec by ref and value.
+    pub(super) fn extend_right<'a, T, A>(x: A, idx: isize, mode: Mode, cval: T) -> T
+    where
+        T: Num + Neg<Output = T> + Copy + 'a,
+        isize: Into<T>,
+        A: Into<ArrayView1<'a, T>>,
+    {
+        let x = x.into();
+        let len_x = x.len() as isize;
+        match mode {
+            Mode::Constant => cval,
+            Mode::Symmetric => {
+                if idx < (2 * len_x) {
+                    x[(len_x - 1 - (idx - len_x)) as usize]
+                } else {
+                    let idx = idx % (2 * len_x);
+                    if idx < len_x {
+                        x[idx as usize]
+                    } else {
+                        x[(len_x - 1 - (idx - len_x)) as usize]
+                    }
+                }
+            }
+            Mode::ConstantEdge => x[(len_x - 1) as usize],
+            Mode::Smooth => {
+                let last = x[(len_x - 1) as usize];
+                let second_last = x[(len_x - 2) as usize];
+                last + (idx - len_x + 1).into() * (last - second_last)
+            }
+            Mode::Periodic => x[(idx % len_x) as usize],
+            Mode::Reflect => {
+                if idx < (2 * len_x - 1) {
+                    x[(len_x - 2 - (idx - len_x)) as usize]
+                } else {
+                    let idx = idx % (2 * (len_x - 1));
+                    if idx < (len_x - 1) {
+                        x[idx as usize]
+                    } else {
+                        x[(len_x - 1 - (idx - (len_x - 1))) as usize]
+                    }
+                }
+            }
+            Mode::Antisymmetric => {
+                if idx < (2 * len_x) {
+                    -x[(len_x - 1 - (idx - len_x)) as usize]
+                } else {
+                    let idx = idx % (2 * len_x);
+                    if idx < len_x {
+                        x[idx as usize]
+                    } else {
+                        -x[(len_x - 1 - (idx - len_x)) as usize]
+                    }
+                }
+            }
+            Mode::Antireflect => {
+                if idx < (2 * len_x - 1) {
+                    let last = x[(len_x - 1) as usize];
+                    let u = x[(len_x - 2 - (idx - len_x)) as usize];
+                    last - (u - last)
+                } else {
+                    let last = x[(len_x - 1) as usize];
+                    let first = x[0];
+                    let re = last + (last - first) * ((idx / (len_x - 1)) - 1).into();
+                    let idx = idx % (2 * (len_x - 1));
+                    if idx < (len_x - 1) {
+                        re + (x[idx as usize] - first)
+                    } else {
+                        let u = x[(len_x - 1 - (idx - (len_x - 1))) as usize];
+                        re + (last - u)
+                    }
+                }
+            }
+            Mode::Line => {
+                let first = x[0];
+                let last = x[(len_x - 1) as usize];
+                let lin_slope = (last - first) / (len_x - 1).into();
+                last + ((idx - len_x + 1) as isize).into() * lin_slope
+            }
+        }
+    }
 }

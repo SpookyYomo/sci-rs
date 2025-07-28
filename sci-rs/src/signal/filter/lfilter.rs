301b779bab2b45858acbeddad64008ef0876d280
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-07-28 15:25:36 +0800

    Move lfilter implementation into macro
    Correspondingly update all test cases.
    See previous commit for rationale.

--- sci-rs/src/signal/filter/lfilter.rs
+++ sci-rs/src/signal/filter/lfilter.rs
@@ -119,23 +119,23 @@ where
     ///
     /// # Examples
     /// On a 1-dimensional signal:
-    //// ```
-    //// use ndarray::{array, ArrayBase, Array1, ArrayView1, Dim, Ix, OwnedRepr};
-    //// use sci_rs::signal::filter::LFilter;
-    //// 
-    //// let b = array![5., 4., 1., 2.];
-    //// let a = array![1.];
-    //// let x = array![1., 2., 3., 4., 3., 5., 6.];
-    //// let expected = array![5., 14., 24., 36., 38., 47., 61.];
-    //// let (result, _) = ArrayView1::lfilter((&b).into(), (&a).into(), (&x).into(), None, None).unwrap(); // By ref
-    //// 
-    //// assert_eq!(result.len(), expected.len());
-    //// result.into_iter().zip(expected).for_each(|(r, e)| {
-    ////     assert_eq!(r, e);
-    //// });
-    //// 
-    //// let (result, _) = Array1::lfilter((&b).into(), (&a).into(), x, None, None).unwrap(); // By value
-    //// ```
+    /// ```
+    /// use ndarray::{array, ArrayBase, Array1, ArrayView1, Dim, Ix, OwnedRepr};
+    /// use sci_rs::signal::filter::LFilter;
+    ///
+    /// let b = array![5., 4., 1., 2.];
+    /// let a = array![1.];
+    /// let x = array![1., 2., 3., 4., 3., 5., 6.];
+    /// let expected = array![5., 14., 24., 36., 38., 47., 61.];
+    /// let (result, _) = ArrayView1::lfilter((&b).into(), (&a).into(), (&x).into(), None, None).unwrap(); // By ref
+    ///
+    /// assert_eq!(result.len(), expected.len());
+    /// result.into_iter().zip(expected).for_each(|(r, e)| {
+    ///     assert_eq!(r, e);
+    /// });
+    ///
+    /// let (result, _) = Array1::lfilter((&b).into(), (&a).into(), x, None, None).unwrap(); // By value
+    /// ```
     ///
     /// # Panics
     /// Currently yet to implement for `zi = Some(...)`, nor for `a.len() > 1`.
@@ -156,211 +156,152 @@ where
         S: Data<Elem = T> + 'a;
 }
 
-/// Filter data along one-dimension with an IIR or FIR filter.
-///
-/// Filter a data sequence, `x`, using a digital filter.  This works for many
-/// fundamental data types (including Object type).  The filter is a direct
-/// form II transposed implementation of the standard difference equation
-/// (see Notes).
-///
-/// The function [super::sosfilt] (and filter design using ``output='sos'``) should be
-/// preferred over `lfilter` for most filtering tasks, as second-order sections
-/// have fewer numerical problems.
-///
-/// ## Parameters
-/// * `b` : array_like  
-///   The numerator coefficient vector in a 1-D sequence.
-/// * `a` : array_like  
-///   The denominator coefficient vector in a 1-D sequence.  If ``a[0]``
-///   is not 1, then both `a` and `b` are normalized by ``a[0]``.
-/// * `x` : array_like  
-///   An N-dimensional input array.
-/// * `axis`: Option<isize>
-///   Default to `-1` if `None`.  
-///   Panics in accordance with [ndarray::ArrayBase::axis_iter].
-/// * `zi`: array_like  
-///   Currently not implemented.  
-///   Initial conditions for filter delays. It is a vector
-///   (or array of vectors for an N-dimensional input) of length
-///   ``max(len(a), len(b)) - 1``.  If `zi` is None or is not given then
-///   initial rest is assumed.  See `lfiltic` and [super::lfilter_zi] for more information.
-///
-/// ## Returns
-/// * `y` : array  
-///   The output of the digital filter.
-/// * `zf` : array, optional  
-///   If `zi` is None, this is not returned, otherwise, `zf` holds the
-///   final filter delay values.
-///
-/// # See Also
-/// * [super::lfilter_zi]  
-///
-/// # Notes
-///
-/// # Examples
-/// On a 1-dimensional signal:
-/// ```
-/// use ndarray::{array, ArrayBase, Dim, Ix, OwnedRepr};
-/// use sci_rs::signal::filter::lfilter;
-///
-/// let b = array![5., 4., 1., 2.];
-/// let a = array![1.];
-/// let x = array![1., 2., 3., 4., 3., 5., 6.];
-/// let expected = array![5., 14., 24., 36., 38., 47., 61.];
-/// let (result, _) = lfilter((&b).into(), (&a).into(), x, None, None).unwrap();
-///
-/// assert_eq!(result.len(), expected.len());
-/// result.into_iter().zip(expected).for_each(|(r, e)| {
-///     assert_eq!(r, e);
-/// })
-/// ```
-///
-/// # Panics
-/// Currently yet to implement for `zi = Some(...)`, nor for `a.len() > 1`.
-/// Panics if axis is out or range.
-// NOTE: zi's TypeSig inherits from lfilter's output, in accordance with examples section of
-// documentation, both lfilter_zi and this should eventually support NDArray.
-pub fn lfilter<'a, T, S, const N: usize>(
-    b: ArrayView1<'a, T>,
-    a: ArrayView1<'a, T>,
-    x: ArrayBase<S, Dim<[Ix; N]>>,
-    axis: Option<isize>,
-    zi: Option<ArrayView<T, Dim<[Ix; N]>>>,
-) -> Result<(Array<T, Dim<[Ix; N]>>, Option<Array<T, Dim<[Ix; N]>>>)>
-where
-    [Ix; N]: IntoDimension<Dim = Dim<[Ix; N]>>,
-    Dim<[Ix; N]>: RemoveAxis,
-    T: NumAssign + FromPrimitive + Copy + 'a,
-    S: Data<Elem = T> + 'a,
-{
-    if N == 0 {
-        // `_validate_x` condition - ndarray allows for 0-dimensional arrays
-        return Err(Error::InvalidArg {
-            arg: "x".into(),
-            reason: "Linear filter requires at least 1-dimensional `x`.".into(),
-        });
-    }
-
-    if a.len() > 1 {
-        return linear_filter(b, a, x, axis, zi);
-    };
-
-    let (axis, axis_inner) = check_and_get_axis(axis, &x)?;
-
-    if a.is_empty() {
-        return Err(Error::InvalidArg {
-            arg: "a".into(),
-            reason:
-                "Empty 1D array will result in inf/nan result. Consider setting to `array![1.]`."
-                    .into(),
-        });
-    } else if a.first().unwrap().is_zero() {
-        return Err(Error::InvalidArg {
-            arg: "a".into(),
-            reason: "First element of a found to be zero.".into(),
-        });
-    }
-    let b: Array1<T> = b.mapv(|bi| bi / a[0]); // b /= a[0]
-
-    if let Some(mut zi) = zi {
-        // Use a separate branch to avoid unnecessary heap allocation of `out_full` in `zi` = None
-        // case.
-
-        let zi = {
-            // if zi.ndim != x.ndim { return Err(...) } is signature asserted.
-
-            todo!();
-            zi
-        };
-
-        let (out_full_dim, out_full_dim_inner): (Dim<_>, [Ix; N]) = {
-            let mut tmp: [Ix; N] = ndarray_shape_as_array(&x);
-            tmp[axis_inner] += b.len_of(Axis(0)) - 1; // From np.convolve(..., 'full')
-            (IntoDimension::into_dimension(tmp), tmp)
-        };
-
-        let mut out_full: Array<T, Dim<[Ix; N]>> = ArrayBase::zeros(out_full_dim);
-        out_full
-            .lanes_mut(axis)
-            .into_iter()
-            .zip(x.lanes(axis)) // Almost basically np.apply_along_axis
-            .try_for_each(|(mut out_full_slice, y)| {
-                // np.convolve uses full mode by default
-                // ```py
-                // out_full = np.apply_along_axis(lambda y: np.convolve(b, y), axis, x)
-                // ```
-                use sci_rs_core::num_rs::{convolve, ConvolveMode};
-                convolve(y, (&b).into(), ConvolveMode::Full)?.assign_to(&mut out_full_slice);
-                Ok(())
-            })?;
+macro_rules! lfilter_for_dim {
+    ($N:literal) => {
+        impl<S> LFilter<S, $N> for ArrayBase<S, Dim<[Ix; $N]>>
+        where
+            S: ndarray::RawData,
         {
-            // ```py
-            // ind[axis] = slice(zi.shape[axis])
-            // out_full[tuple(ind)] += zi
-            // ```
-            todo!()
-        };
+            fn lfilter<'a, T>(
+                b: ArrayView1<'a, T>,
+                a: ArrayView1<'a, T>,
+                x: Self,
+                axis: Option<isize>,
+                zi: Option<ArrayView<T, Dim<[Ix; $N]>>>,
+            ) -> Result<(Array<T, Dim<[Ix; $N]>>, Option<Array<T, Dim<[Ix; $N]>>>)>
+            where
+                [Ix; $N]: IntoDimension<Dim = Dim<[Ix; $N]>>,
+                Dim<[Ix; $N]>: RemoveAxis,
+                T: NumAssign + FromPrimitive + Copy + 'a,
+                S: Data<Elem = T> + 'a,
+            {
+                if a.len() > 1 {
+                    return linear_filter(b, a, x, axis, zi);
+                };
 
-        let (out_dim, out_dim_inner) = {
-            let mut tmp: [Ix; N] = ndarray_shape_as_array(&x);
-            (IntoDimension::into_dimension(tmp), tmp)
-        };
-        let mut out = ArrayBase::zeros(out_dim);
-        out.lanes_mut(axis)
-            .into_iter()
-            .zip(out_full.lanes(axis))
-            .for_each(|(mut out_slice, out_full_slice)| {
-                // ```py
-                // # Create the [...; :out_full.shape[axis] - len(b) + 1; ...] at index=axis
-                // ind[axis] = slice(out_full.shape[axis] - len(b) + 1)
-                // out = out_full[tuple(ind)]
-                // ```
-                out_full_slice
-                    .slice(
-                        SliceInfo::try_from([SliceInfoElem::Slice {
-                            start: 0,
-                            end: Some(out_dim_inner[axis_inner] as isize),
-                            step: 1,
-                        }])
-                        .unwrap(),
-                    )
-                    .assign_to(&mut out_slice);
-            });
+                let (axis, axis_inner) = check_and_get_axis(axis, &x)?;
 
-        Ok((out, todo!()))
-    } else {
-        let (out_dim, out_dim_inner): (Dim<_>, [Ix; N]) = {
-            let mut tmp: [Ix; N] = ndarray_shape_as_array(&x);
-            (IntoDimension::into_dimension(tmp), tmp)
-        };
-        let mut out = ArrayBase::zeros(out_dim);
+                if a.is_empty() {
+                    return Err(Error::InvalidArg {
+                        arg: "a".into(),
+                        reason:
+                            "Empty 1D array will result in inf/nan result. Consider setting to `array![1.]`."
+                                .into(),
+                    });
+                } else if a.first().unwrap().is_zero() {
+                    return Err(Error::InvalidArg {
+                        arg: "a".into(),
+                        reason: "First element of a found to be zero.".into(),
+                    });
+                }
+                let b: Array1<T> = b.mapv(|bi| bi / a[0]); // b /= a[0]
 
-        out.lanes_mut(axis)
-            .into_iter()
-            .zip(x.lanes(axis)) // Almost basically np.apply_along_axis
-            .try_for_each(|(mut out_slice, y)| {
-                // np.convolve uses full mode, but is eventually slices out with
-                // ```py
-                // ind = out_full.ndim * [slice(None)] # creates the "[:, :, ..., :]" slice r
-                // ind[axis] = slice(out_full.shape[axis] - len(b) + 1) # [:out_full.shape[ ..] - len(b) + 1]
-                // ```
-                use sci_rs_core::num_rs::{convolve, ConvolveMode};
-                let out_full = convolve(y, (&b).into(), ConvolveMode::Full)?;
-                out_full
-                    .slice(
-                        SliceInfo::try_from([SliceInfoElem::Slice {
-                            start: 0,
-                            end: Some(out_dim_inner[axis_inner] as isize),
-                            step: 1,
-                        }])
-                        .unwrap(),
-                    )
-                    .assign_to(&mut out_slice);
-                Ok(())
-            })?;
+                if let Some(zi) = zi {
+                    // Use a separate branch to avoid unnecessary heap allocation of `out_full` in `zi` = None
+                    // case.
 
-        Ok((out, None))
-    }
+                    let zi = {
+                        // if zi.ndim != x.ndim { return Err(...) } is signature asserted.
+
+                        todo!();
+                        zi
+                    };
+
+                    let (out_full_dim, out_full_dim_inner): (Dim<_>, [Ix; $N]) = {
+                        let mut tmp: [Ix; $N] = ndarray_shape_as_array(&x);
+                        tmp[axis_inner] += b.len_of(Axis(0)) - 1; // From np.convolve(..., 'full')
+                        (IntoDimension::into_dimension(tmp), tmp)
+                    };
+
+                    let mut out_full: Array<T, Dim<[Ix; $N]>> = ArrayBase::zeros(out_full_dim);
+                    out_full
+                        .lanes_mut(axis)
+                        .into_iter()
+                        .zip(x.lanes(axis)) // Almost basically np.apply_along_axis
+                        .try_for_each(|(mut out_full_slice, y)| {
+                            // np.convolve uses full mode by default
+                            // ```py
+                            // out_full = np.apply_along_axis(lambda y: np.convolve(b, y), axis, x)
+                            // ```
+                            use sci_rs_core::num_rs::{convolve, ConvolveMode};
+                            convolve(y, (&b).into(), ConvolveMode::Full)?
+                                .assign_to(&mut out_full_slice);
+                            Ok(())
+                        })?;
+                    {
+                        // ```py
+                        // ind[axis] = slice(zi.shape[axis])
+                        // out_full[tuple(ind)] += zi
+                        // ```
+                        todo!()
+                    };
+
+                    let (out_dim, out_dim_inner) = {
+                        let mut tmp: [Ix; $N] = ndarray_shape_as_array(&x);
+                        (IntoDimension::into_dimension(tmp), tmp)
+                    };
+                    let mut out = ArrayBase::zeros(out_dim);
+                    out.lanes_mut(axis)
+                        .into_iter()
+                        .zip(out_full.lanes(axis))
+                        .for_each(|(mut out_slice, out_full_slice)| {
+                            // ```py
+                            // # Create the [...; :out_full.shape[axis] - len(b) + 1; ...] at index=axis
+                            // ind[axis] = slice(out_full.shape[axis] - len(b) + 1)
+                            // out = out_full[tuple(ind)]
+                            // ```
+                            out_full_slice
+                                .slice(
+                                    SliceInfo::try_from([SliceInfoElem::Slice {
+                                        start: 0,
+                                        end: Some(out_dim_inner[axis_inner] as isize),
+                                        step: 1,
+                                    }])
+                                    .unwrap(),
+                                )
+                                .assign_to(&mut out_slice);
+                        });
+
+                    Ok((out, todo!()))
+                } else {
+                    // In contrast to the case where zi.is_some(), we can inline a slicing operation to reduce
+                    // one extra heap allocation.
+
+                    let (out_dim, out_dim_inner): (Dim<_>, [Ix; $N]) = {
+                        let mut tmp: [Ix; $N] = ndarray_shape_as_array(&x);
+                        (IntoDimension::into_dimension(tmp), tmp)
+                    };
+                    let mut out = ArrayBase::zeros(out_dim);
+
+                    out.lanes_mut(axis)
+                        .into_iter()
+                        .zip(x.lanes(axis)) // Almost basically np.apply_along_axis
+                        .try_for_each(|(mut out_slice, y)| {
+                            // np.convolve uses full mode, but is eventually slices out with
+                            // ```py
+                            // ind = out_full.ndim * [slice(None)] # creates the "[:, :, ..., :]" slice r
+                            // ind[axis] = slice(out_full.shape[axis] - len(b) + 1) # [:out_full.shape[ ..] - len(b) + 1]
+                            // ```
+                            use sci_rs_core::num_rs::{convolve, ConvolveMode};
+                            let out_full = convolve(y, (&b).into(), ConvolveMode::Full)?;
+                            out_full
+                                .slice(
+                                    SliceInfo::try_from([SliceInfoElem::Slice {
+                                        start: 0,
+                                        end: Some(out_dim_inner[axis_inner] as isize),
+                                        step: 1,
+                                    }])
+                                    .unwrap(),
+                                )
+                                .assign_to(&mut out_slice);
+                            Ok(())
+                        })?;
+
+                    Ok((out, None))
+                }
+            }
+        }
+    };
 }
 
 /// Internal function called by [lfilter] for situation a.len() > 1.
@@ -380,6 +321,13 @@ where
     todo!()
 }
 
+lfilter_for_dim!(1);
+lfilter_for_dim!(2);
+lfilter_for_dim!(3);
+lfilter_for_dim!(4);
+lfilter_for_dim!(5);
+lfilter_for_dim!(6);
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -397,7 +345,8 @@ mod test {
             let x = array![1., 2., 3., 4., 3., 5., 6.];
             let expected = array![5., 14., 24., 36., 38., 47., 61.];
 
-            let Ok((result, None)) = lfilter((&b).into(), (&a).into(), x, None, None) else {
+            let Ok((result, None)) = Array1::lfilter((&b).into(), (&a).into(), x, None, None)
+            else {
                 panic!("Should not have errored")
             };
 
@@ -413,7 +362,8 @@ mod test {
             let x = array![1., 2., 3., 4., 3., 5., 6.];
             let expected = array![0.7, 1.1, 2.1, 3.1, 2.7, 5., 4.5];
 
-            let Ok((result, None)) = lfilter((&b).into(), (&a).into(), x, None, None) else {
+            let Ok((result, None)) = Array1::lfilter((&b).into(), (&a).into(), x, None, None)
+            else {
                 panic!("Should not have errored")
             };
 
@@ -430,19 +380,19 @@ mod test {
         let a = array![1.];
         let x = array![1., 2., 3., 4., 3., 5., 6.];
 
-        let result = lfilter((&b).into(), (&a).into(), x.clone(), Some(2), None);
+        let result = ArrayView1::lfilter((&b).into(), (&a).into(), (&x).into(), Some(2), None);
         assert!(result.is_err());
 
-        let result = lfilter((&b).into(), (&a).into(), x.clone(), Some(1), None);
+        let result = Array1::lfilter((&b).into(), (&a).into(), x.clone(), Some(1), None);
         assert!(result.is_err());
 
-        let result = lfilter((&b).into(), (&a).into(), x.clone(), Some(0), None);
+        let result = Array1::lfilter((&b).into(), (&a).into(), x.clone(), Some(0), None);
         assert!(result.is_ok());
 
-        let result = lfilter((&b).into(), (&a).into(), x.clone(), Some(-1), None);
+        let result = Array1::lfilter((&b).into(), (&a).into(), x.clone(), Some(-1), None);
         assert!(result.is_ok());
 
-        let result = lfilter((&b).into(), (&a).into(), x, Some(-2), None);
+        let result = Array1::lfilter((&b).into(), (&a).into(), x, Some(-2), None);
         assert!(result.is_err());
     }
 }

c72cbd6950637bcf5cae6e1af5f10110497f69ba
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-09-29 16:54:19 +0800

    Add axis_slice function with bad signature
    The functionality currently works, but the signature cannot explicitly
    specify the return type to be `D`. This is made possible by using heap
    allocation of the `SliceInfo` through `Vec` instead of using the
    macro/trait method in Lfilter, in an attempt have another method to
    circumvent the need for a nightly compiler that uses similar trait
    bounds as NDArray without needing a nightly compiler.
    
    This function is eventually used in even/odd/const extensions (which has
    only been implemented on nalgebra arrays and not NDArrays) so far.

--- sci-rs/src/signal/filter/arraytools.rs
+++ sci-rs/src/signal/filter/arraytools.rs
@@ -2,7 +2,11 @@
 //!
 //! Designed for ndarrays; with scipy's internal nomenclature.
 
-use ndarray::{ArrayBase, Axis, Data, Dim, Dimension, IntoDimension, Ix, RemoveAxis};
+use alloc::{vec, vec::Vec};
+use ndarray::{
+    ArrayBase, ArrayView, Axis, Data, Dim, Dimension, IntoDimension, Ix, RemoveAxis, SliceArg,
+    SliceInfo, SliceInfoElem,
+};
 use sci_rs_core::{Error, Result};
 
 /// Internal function for casting into [Axis] and appropriate usize from isize.
@@ -110,4 +114,104 @@ where
     S: Data<Elem = T> + 'a,
 {
     a.shape().try_into().expect("Could not cast shape to array")
+}
+
+/// Takes a slice along `axis` from `a`.
+///
+/// # Parameters
+/// * `a`: Array being sliced from.
+/// * `start`: `Option<isize>`. None defaults to 0.
+/// * `end`: `Option<isize>`.
+/// * `step`: `Option<isize>`. None default to 1.
+/// * `axis`: `Option<isize>`. None defaults to -1.
+///
+/// # Errors
+/// - Axis is out of bounds.
+/// - Start/stop elements are out of bounds.
+///
+pub fn axis_slice<A, S, D>(
+    a: &ArrayBase<S, D>,
+    start: Option<isize>,
+    end: Option<isize>,
+    step: Option<isize>,
+    axis: Option<isize>,
+) -> Result<ArrayView<'_, A, <SliceInfo<Vec<SliceInfoElem>, D, D> as SliceArg<D>>::OutDim>>
+where
+    S: Data<Elem = A>,
+    D: Dimension,
+    SliceInfo<Vec<SliceInfoElem>, D, D>: SliceArg<D>,
+{
+    if D::NDIM.is_none() {
+        return Err(Error::InvalidArg {
+            arg: 'a'.into(),
+            reason: "IxDyn array is not supported".into(),
+        });
+    }
+    #[allow(non_snake_case)]
+    let N = D::NDIM.unwrap();
+
+    // Axis object and its corresponding usize internal.
+    let (axis, axis_inner) = {
+        if axis.is_some_and(|axis| {
+            !(if axis < 0 {
+                axis.unsigned_abs() <= D::NDIM.unwrap()
+            } else {
+                axis.unsigned_abs() < D::NDIM.unwrap()
+            })
+        }) {
+            return Err(Error::InvalidArg {
+                arg: "axis".into(),
+                reason: "index out of range.".into(),
+            });
+        }
+
+        // We make a best effort to convert into appropriate axis object.
+        let axis_inner: isize = axis.unwrap_or(-1);
+        if axis_inner >= 0 {
+            (Axis(axis_inner as usize), axis_inner.unsigned_abs())
+        } else {
+            let axis_inner = a
+                .ndim()
+                .checked_add_signed(axis_inner)
+                .expect("Invalid add to `axis` option");
+            (Axis(axis_inner), axis_inner)
+        }
+    };
+
+    let sl = SliceInfo::<_, D, D>::try_from({
+        let mut tmp = vec![
+            SliceInfoElem::Slice {
+                start: 0,
+                end: None,
+                step: 1,
+            };
+            N
+        ];
+        tmp[axis_inner] = SliceInfoElem::Slice {
+            start: start.unwrap_or(0),
+            end,
+            step: step.unwrap_or(1),
+        };
+
+        tmp
+    })
+    .unwrap();
+
+    Ok(a.slice(&sl))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ndarray::array;
+
+    #[test]
+    fn axis_slice_doc() {
+        let a = array![[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+        assert_eq!(
+            axis_slice(&a, Some(0), Some(1), Some(1), Some(1)).unwrap(),
+            array![[1], [4], [7]]
+        );
+    }
 }
